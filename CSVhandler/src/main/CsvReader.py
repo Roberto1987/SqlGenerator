8c50f938eba999ecf5a55226fc4e53b924e9c946
Roberto1987 <roberto.testa1987@gmail.com>
2018-02-07 10:36:05 +0000

    small commit

--- CSVhandler/src/main/CsvReader.py
+++ CSVhandler/src/main/CsvReader.py
@@ -10,8 +10,17 @@ from src.main.CsvRetriever import csvFromTextAcquisition
 
 class CsvReader:
 
+    # Initializing:
+    #   configManager: read all the props from queryCreator.ini
+    #   timestamp : timestamp
+    #   humanTimestamp : timestamp in a human readable format
+    #   delimitator: log formatting element
+    #   sourcePath : path of the source file
+    #   insertStatement: first INSERT's query statement
+    #   outputDirectoryPath: path of the output file
+    #   fileName : name of the output file
+
     def __init__(self):
-        self.configManager = ConfigManager()
         self.timestamp = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S'))
         self.humanTimestamp = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
         self.delimitator = ' =========================== '
@@ -21,40 +30,49 @@ class CsvReader:
         logging.info("\n --- Initialization ---")
         logging.info("\n --- Loading configs from .ini file --- \n")
 
+        self.configManager = ConfigManager()
+
         self.sourcePath = os.path.join(os.path.join(
-                         self.configManager.RELATIVE_PATH,
-                         self.configManager.resourceFolder),
-                                self.configManager.filename)
+            self.configManager.RELATIVE_PATH,
+            self.configManager.resourceFolder),
+            self.configManager.filename
+        )
 
         self.insertStatement = "INSERT INTO " + self.configManager.nameTable + self.configManager.firstInsertCmd
 
-        dirName = os.path.join('..', self.configManager.outputFolder)
-        logging.info('Selected output directory:' + dirName)
-        fileName = 'Inserts' + self.timestamp + '.txt'
-        path = os.path.join(dirName, fileName)
+        outputPath = os.path.join('..', self.configManager.outputFolder)
+        self.fileName = 'Inserts' + self.timestamp + '.txt'
+        path = os.path.join(outputPath, self.fileName)
+
+        logging.info('Selected output directory:' + outputPath)
         logging.info('Path of the written file path:' + path)
 
-        self.fileInserts = open(path, 'w', encoding='utf-8')
+        self.outputFile = open(path, 'w', encoding='utf-8')
 
+    # input: string matrix 'data'
+    #
+    # Creation of a batch of inserts, looping between each row of the 'data' matrix
+    # writing it on a file
     def queryCreation(self, data):
         for i in range(0, data.shape[0]):
-            self.fileInserts.write(
+            self.outputFile.write(
                 self.configManager.START_BRACKET + data[i, 1] + "," + self.configManager.loca_id + "," +
                 self.configManager.APEX + data[i, 6] + self.configManager.APEX + "," +
                 self.configManager.APEX + data[i, 3] + self.configManager.APEX +
                 self.configManager.END_BRACKET + '\n'
             )
 
-        logging.info(self.delimitator + str(i + 1) + ' rows processed. Process ended.' + self.delimitator)
+        logging.info(self.delimitator + str(i + 1) + ' row written. Process ended.' + self.delimitator)
         print('query writings ended')
 
+    #
+    # output: A string's matrix created from the csv file
+    #
     def openCsv(self):
-        X = csvFromTextAcquisition(self.sourcePath, int(self.configManager.cols));
-        xShape = np.shape(X)
-        logging.info('The matrix produced from the CSV has shape ' + str(xShape))  #
-
-        self.fileInserts.write(self.configManager.firstInsertCmd + self.configManager.VALUES + '\n')
-        return X
+        csvMatrix = csvFromTextAcquisition(self.sourcePath, int(self.configManager.cols));
+        self.outputFile.write(self.configManager.firstInsertCmd + self.configManager.VALUES + '\n')
+        logging.info('The matrix produced from the CSV has shape ' + str(np.shape(csvMatrix)))
+        return csvMatrix
 
 
 reader = CsvReader()
--- CSVhandler/src/main/CsvRetriever.py
+++ CSVhandler/src/main/CsvRetriever.py
@@ -2,13 +2,22 @@ import numpy as np
 import logging
 
 
+#  Input:
+#       path: path of the csv file to import
+#       cols: number of column of the csv need to be taken
+#  Csv import as text in a matrix form
+import sys
+
+
 def csvFromTextAcquisition(path, cols):
     try:
         X = np.genfromtxt(
             path, delimiter=",", encoding='utf-8',
             skip_header=True, autostrip=True, dtype=None,
             usecols=np.arange(0, cols))
-    except:
-        logging.warning("File " + path + " not found")
+    except IOError:
+        logging.error("File " + path + " not found")
+        print("File " + path + " not found")
+        sys.exit()
 
     return X
